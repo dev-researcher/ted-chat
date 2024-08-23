60062d3156912a4685e627c4aca9fbabb1c82185
Dev-Researcher <174758270+dev-researcher@users.noreply.github.com>
2024-08-22 21:39:26 -0600

    deleting files

--- config.py
+++
@@ -1,5 +0,0 @@
-# PROJECT_ID = 'mychat-433121' 
-PROJECT_ID = 'formal-incline-431300-v8'
-REGION = 'us-central1'
-MODEL_NAME = "gemini-1.0-pro-002"
-# MODEL_NAME = 'publishers/google/models/gemini-pro'
